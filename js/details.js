61bff60c5ad211ba30918c4696349af05c0b7eb3
An9v0s7 <anirudh_sundar@gap.com>
2015-08-28 17:12:15 +0530

    Add javascript code for dynamically populating fields

---
+++ js/details.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+	var passedItemId = GLOB_SITE.getUrlParameter("item_id");
+	for (var i = 0; i < json.category.length; i++){
+		for (var j = 0; j < json.category[i].items.length; j++){
+			var itemName = json.category[i].items[j].item_name;
+			var itemId = json.category[i].items[j].sku;
+			var itemPrice = json.category[i].items[j].price;
+			var productDesc = json.category[i].items[j].description;
+			if (passedItemId === itemId) {
+				$("#productTitle").html(itemName.toUpperCase());
+				$("#productPrice").html(itemPrice);
+				$("#productDesc").html(productDesc);
+			}
+		}
+	}
+});
