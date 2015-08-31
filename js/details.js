568593602830ecb75314380066a466877d70fcfa
Anirudh Sundar <anirudh.sundar@gmail.com>
2015-08-31 14:12:26 +0530

    Modify code to fetch the new json

--- js/details.js
+++ js/details.js
@@ -2,17 +2,17 @@ $(document).ready(function(){
 	$("#siteTitle").html(json.website_name);
 	$("footer.site-title").html("&copy; "+json.website_name);
 	var passedItemId = GLOB_SITE.getUrlParameter("item_id");
-	for (var i = 0; i < json.category.length; i++){
-		for (var j = 0; j < json.category[i].items.length; j++){
-			var itemName = json.category[i].items[j].item_name;
-			var itemId = json.category[i].items[j].sku;
-			var itemPrice = json.category[i].items[j].price;
-			var productDesc = json.category[i].items[j].description;
+	for (var i = 0; i < json.rows.length; i++){
+		// for (var j = 0; j < json.category[i].items.length; j++){
+			var itemName = json.rows[i].doc.name;
+			var itemId = json.rows[i].doc["_id"];
+			var itemPrice = json.rows[i].doc.usaDollarPrice;
+			var productDesc = json.rows[i].doc.description;
 			if (passedItemId === itemId) {
 				$("#productTitle").html(itemName.toUpperCase());
 				$("#productPrice").html(itemPrice);
 				$("#productDesc").html(productDesc);
 			}
-		}
+		// }
 	}
 });
--- js/main.js
+++ js/main.js
@@ -9,31 +9,31 @@ $(document).ready(function(){
 			json = data;
 		}
 	});*/
-	var productsListElements = "";
-	var productsGridElements = "";
-	$("#siteTitle").html(json.website_name);
+var productsListElements = "";
+var productsGridElements = "";
+	// $("#siteTitle").html(json.website_name);
 	$("footer.site-title").html("&copy; "+json.website_name);
 
-	for (var i = 0; i < json.category.length; i++){
-		productsListElements = productsListElements + '<li class="product-category"><a href="#">'+json.category[i].cat_name.toUpperCase()+'</a></li>';
-		for (var j = 0; j < json.category[i].items.length; j++){
-			var itemName = json.category[i].items[j].item_name;
-			var itemId = json.category[i].items[j].sku;
-			var itemPrice = json.category[i].items[j].price;
+	for (var i = 0; i < json.rows.length; i++){
+		// productsListElements = productsListElements + '<li class="product-category"><a href="#">'+json.category[i].cat_name.toUpperCase()+'</a></li>';
+		// for (var j = 0; j < json.rows[i].length; j++){
+			var itemName = json.rows[i].doc.name;
+			var itemId = json.rows[i].doc["_id"];
+			var itemPrice = json.rows[i].doc.usaDollarPrice;
 			productsGridElements = productsGridElements + '<div class="col-md-4 col-xs-6 product-thumb-container"><a href="details.html?item_id='+itemId+'"><img src="images/label15.png" alt="Product" class="product-image"><div class="product-name">'+itemName+'</div><div class="product-name">'+itemPrice+'</div></a></div>';
-		}
+		// }
 	}
 	$("#productsGrid").html(productsGridElements);
-	$("#productsList").html(productsListElements);
+	// $("#productsList").html(productsListElements);
 
 	$(".product-category").on('click',function(){
 		productsGridElements = "";
 		for (var i = 0; i < json.category.length; i++){
 			if (json.category[i].cat_name.toUpperCase() === $(this).text()){
 				for (var j = 0; j < json.category[i].items.length; j++){
-					var itemName = json.category[i].items[j].item_name;
-					var itemId = json.category[i].items[j].sku;
-					var itemPrice = json.category[i].items[j].price;
+					var itemName = json.rows[i].doc.name;
+					var itemId = json.rows[i].doc["_id"];
+					var itemPrice = json.rows[i].doc.usaDollarPrice;
 					productsGridElements = productsGridElements + '<div class="col-md-4 col-xs-6 product-thumb-container"><a href="details.html?item_id='+itemId+'"><img src="images/label15.png" alt="Product" class="product-image"><div class="product-name">'+itemName+'</div><div class="product-name">'+itemPrice+'</div></a></div>';
 				}
 			}
